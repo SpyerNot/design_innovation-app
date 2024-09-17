6c260e455b0695992241f4634506e079d5194694
SpyerNot <low_li_wen@students.edu.sg>
2024-09-17 19:28:57 +0800

    Update homepage.py

---
+++ homepage.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.page_link("homepage.py", label="Home", icon="🏠")
+st.page_link("pages/survey.py", label="Survey", icon="1️⃣")
+st.page_link("pages/rewards.py", label="Page 2", icon="2️⃣")
