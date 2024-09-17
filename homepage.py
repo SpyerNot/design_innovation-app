506d9a7a50381862e594bd4a22b2f55f2e385619
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 03:13:25 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -4,6 +4,5 @@ st.page_link("homepage.py", label="Home", icon="🏠")
 st.page_link("pages/survey.py", label="Survey", icon="1️⃣")
 st.page_link("pages/rewards.py", label="Rewards", icon="2️⃣")
 st.page_link("pages/game.py",label="Games")
-pg = st.navigation([st.Page('pages/survey.py'), st.Page("page2.py")])
+pg = st.navigation([st.Page(page1), st.Page("page2.py")])
 pg.run()
-
