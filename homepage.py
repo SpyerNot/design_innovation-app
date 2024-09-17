6ffa3fae2f83ad8dc42311c77b26ba36f581b095
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 03:12:32 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from page_functions import page1
 st.page_link("homepage.py", label="Home", icon="🏠")
 st.page_link("pages/survey.py", label="Survey", icon="1️⃣")
 st.page_link("pages/rewards.py", label="Rewards", icon="2️⃣")
 st.page_link("pages/game.py",label="Games")
+pg = st.navigation([st.Page('pages/survey.py'), st.Page("page2.py")])
+pg.run()
 
