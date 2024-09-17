d7971134d0a3b4192c0f97f7f1071ad7084f8bdb
SpyerNot <low_li_wen@students.edu.sg>
2024-09-17 19:36:37 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -3,3 +3,5 @@ st.page_link("homepage.py", label="Home", icon="🏠")
 st.page_link("pages/survey.py", label="Survey", icon="1️⃣")
 st.page_link("pages/rewards.py", label="Rewards", icon="2️⃣")
 st.page_link("pages/game.py",label="Games")
+with st.sidebar:
+    st.[element_name]
