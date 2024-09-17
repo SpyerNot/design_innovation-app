f2474f4ecf79e74e87ec1231965d1b86f066b245
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 03:17:51 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,4 +1,6 @@
 import streamlit as st
+st.header("Our Health App!!")
+st.columns(1,vertical_alignment="top")
 st.page_link("homepage.py", label="Home", icon="🏠")
 st.page_link("pages/survey.py", label="Survey", icon="1️⃣")
 st.page_link("pages/rewards.py", label="Rewards", icon="2️⃣")
