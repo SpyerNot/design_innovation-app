fc602717da7430c3b6798aaac7aef3c8702234c5
SpyerNot <low_li_wen@students.edu.sg>
2024-09-17 19:32:38 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.page_link("homepage.py", label="Home", icon="🏠")
 st.page_link("pages/survey.py", label="Survey", icon="1️⃣")
 st.page_link("pages/rewards.py", label="Rewards", icon="2️⃣")
-st.page_link("pages/game.py",label="Games",icon=":sunglasses:")
+st.page_link("pages/game.py",label="Games")
