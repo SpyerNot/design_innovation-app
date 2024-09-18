d6ee8b6a87e339ed0b1d2f950129acff87152847
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 09:01:27 +0800

    Update rewards.py

--- pages/rewards.py
+++ pages/rewards.py
@@ -7,8 +7,8 @@ with col1:
     st.image("https://d1csarkz8obe9u.cloudfront.net/posterpreviews/breakfast-food-voucher-design-template-22ff8b1bc7e11ecad0fbab76ced8bceb_screen.jpg")
 
 with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
+    st.header("Fitness voucher")
+    st.image("https://img.freepik.com/free-psd/social-media-promo-gym-fitness_23-2149534335.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717372800&semt=ais_user")
 
 with col3:
     st.header("An owl")
