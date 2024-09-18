8563e3d4a0f86b0c8b3078ab4d745acd9aacba4a
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 09:08:21 +0800

    Update rewards.py

--- pages/rewards.py
+++ pages/rewards.py
@@ -11,5 +11,5 @@ with col2:
     st.image("https://img.freepik.com/free-psd/social-media-promo-gym-fitness_23-2149534335.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717372800&semt=ais_user")
 
 with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+    st.header("Wellness Programme")
+    st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.myheart.org.sg%2F&psig=AOvVaw1uJCMG4EpuLKw8NVwQJu6C&ust=1726708049558000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOis_e2my4gDFQAAAAAdAAAAABAE")
