549acde0427ba5bec8fde6520aa440bc3bbdb574
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 09:14:17 +0800

    Update rewards.py

--- pages/rewards.py
+++ pages/rewards.py
@@ -8,7 +8,7 @@ with col1:
     if st.button("Purchase"):
         st.write("Added to cart")
     else:
-        st.write(-)
+        st.write("-")
 with col2:
     st.header("Fitness voucher")
     st.image("https://img.freepik.com/free-psd/social-media-promo-gym-fitness_23-2149534335.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717372800&semt=ais_user")
