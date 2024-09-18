5a24c3a3d1ead2e75a1eff2eef7b0d594b06426e
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 08:42:47 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -4,5 +4,6 @@ st.page_link("homepage.py", label="Home", icon="🏠")
 st.page_link("pages/survey.py", label="Survey", icon="1️⃣")
 st.page_link("pages/rewards.py", label="Rewards", icon="2️⃣")
 st.page_link("pages/game.py",label="Games")
+st.divider()
 st.subheader("Know more about us")
 
