58c9386f9bae662cde51a21b674c8fbfedabdae5
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 08:54:39 +0800

    Update rewards.py

--- pages/rewards.py
+++ pages/rewards.py
@@ -1,2 +1,15 @@
 import streamlit as st
 st.title("REWARDS!!")
+ol1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Food voucher")
+    st.image("https://d1csarkz8obe9u.cloudfront.net/posterpreviews/breakfast-food-voucher-design-template-22ff8b1bc7e11ecad0fbab76ced8bceb_screen.jpg?ts=1638878570")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
