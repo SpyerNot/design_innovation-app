537fe7c32f490d8d497fe78ba327a8ce170bc677
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 08:56:41 +0800

    Update rewards.py

--- pages/rewards.py
+++ pages/rewards.py
@@ -4,7 +4,7 @@ ol1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("Food voucher")
-    st.image("https://d1csarkz8obe9u.cloudfront.net/posterpreviews/breakfast-food-voucher-design-template-22ff8b1bc7e11ecad0fbab76ced8bceb_screen.jpg?ts=1638878570")
+    st.image("https://d1csarkz8obe9u.cloudfront.net/posterpreviews/breakfast-food-voucher-design-template-22ff8b1bc7e11ecad0fbab76ced8bceb_screen.jpg")
 
 with col2:
     st.header("A dog")
