3e8066a8eaf3d22d3b6ac5a378059c6f77bf2255
SpyerNot <low_li_wen@students.edu.sg>
2024-09-18 09:16:31 +0800

    Update rewards.py

--- pages/rewards.py
+++ pages/rewards.py
@@ -12,7 +12,16 @@ with col1:
 with col2:
     st.header("Fitness voucher")
     st.image("https://img.freepik.com/free-psd/social-media-promo-gym-fitness_23-2149534335.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717372800&semt=ais_user")
+    if st.button("Purchase"):
+        st.write("Added to cart")
+    else:
+        st.write("-")
 
 with col3:
     st.header("Wellness Programme")
     st.image("https://tickikids.ams3.cdn.digitaloceanspaces.com/z2.cache/gallery/organizations/2208/image_5aa5230acca5b9.93285471.jpg")
+    if st.button("Purchase"):
+        st.write("Added to cart")
+    else:
+        st.write("-")
+
