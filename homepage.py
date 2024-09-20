9dc11ce6a224c352be8889dab2971f521af96596
SpyerNot <low_li_wen@students.edu.sg>
2024-09-20 12:07:01 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -3,7 +3,7 @@ st.header("Our Health App!!")
 st.page_link("homepage.py", label="Home", icon="🏠")
 st.page_link("pages/survey.py", label="Survey (Must do first)", icon="1️⃣")
 st.page_link("pages/rewards.py", label="Rewards", icon="2️⃣")
-st.page_link("pages/game.py",label="Games")
+st.page_link("pages/game.py",label="Games",icon="🎮")
 st.divider()
 st.subheader("Know more about us")
 
